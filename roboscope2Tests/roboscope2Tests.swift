a6b74ff6abaf3eb0712625a7dafca5354a6da100
Andriy Yeroshevych <eandrey.com@gmail.com>
2025-10-14 19:08:55 +0300

    Initial Commit

---
+++ roboscope2Tests/roboscope2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  roboscope2Tests.swift
+//  roboscope2Tests
+//
+//  Created by Andrii Ieroshevych on 14.10.2025.
+//
+
+import Testing
+@testable import roboscope2
+
+struct roboscope2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ roboscope2UITests/roboscope2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  roboscope2UITestsLaunchTests.swift
+//  roboscope2UITests
+//
+//  Created by Andrii Ieroshevych on 14.10.2025.
+//
+
+import XCTest
+
+final class roboscope2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
